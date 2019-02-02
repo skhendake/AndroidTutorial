7ab732d30685dcc4393dadca8413e84fba2e269c
Kajal <kajalthorat2406@gmail.com>
2019-02-02 09:40:16 +0530

    changes in main file

--- app/src/main/java/com/fsp/androidtutorial/MainActivity.java
+++ app/src/main/java/com/fsp/androidtutorial/MainActivity.java
@@ -31,6 +31,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
     }
 
     @Override
