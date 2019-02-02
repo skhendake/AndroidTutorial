e55e371a0c361a4bcba6cf64cba8078e2c823ba0
Kajal <kajalthorat2406@gmail.com>
2019-02-02 10:32:38 +0530

    chnges

--- app/src/main/java/com/fsp/androidtutorial/MainActivity.java
+++ app/src/main/java/com/fsp/androidtutorial/MainActivity.java
@@ -24,9 +24,9 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 
-        
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        DrawerLayout drawer =                (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
