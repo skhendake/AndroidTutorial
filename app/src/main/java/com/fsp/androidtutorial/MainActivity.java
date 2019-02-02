2e1d41bec5dde6833dbeb52c2cca5e9f877062e9
Kajal <kajalthorat2406@gmail.com>
2019-02-02 10:36:40 +0530

    chnges file

--- app/src/main/java/com/fsp/androidtutorial/MainActivity.java
+++ app/src/main/java/com/fsp/androidtutorial/MainActivity.java
@@ -17,16 +17,16 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+                protected void onCreate(Bundle savedInstanceState) {
+                 super.onCreate(savedInstanceState);
+                  setContentView(R.layout.activity_main);
+                  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+                 setSupportActionBar(toolbar);
 
 
 
 
-        DrawerLayout drawer =                (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =                      (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
